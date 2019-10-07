56c85b6e709d3cc42d35f493d453e96217f4ace2
Vanessa Tarí Costa <vanessatari@gmail.com>
2019-10-07 17:27:09 +0200

    Creados el formulario de personal data

--- src/app/views/profile-personal-data-form/profile-personal-data-form.component.ts
+++ src/app/views/profile-personal-data-form/profile-personal-data-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-profile-personal-data-form',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-personal-data-form.component.scss']
 })
 export class ProfilePersonalDataFormComponent implements OnInit {
-
-  constructor() { }
+    imageURL: String;
+    user: String;
+    id: any;
+    constructor(private activatedRoute: ActivatedRoute,
+        private toastService: ToastrService, private router: Router) {
+        this.imageURL = "../assets/img/profile.png";
+        this.id = this.activatedRoute.snapshot.params.id;
+        this.user = JSON.parse(localStorage.getItem("currentUser"));
+    }
 
   ngOnInit() {
   }
